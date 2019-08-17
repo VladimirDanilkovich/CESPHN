92f4e6971e4824beb7c8f4e5d2f21476b5f1434a
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-17 20:05:17 +0300

    Update Census.js

--- Census.js
+++ Census.js
@@ -36,27 +36,27 @@ describe('Census', function() {
     await driver.findElement(By.id("Public Transport")).click()
     {
       const dropdown = await driver.findElement(By.id("Public Transport"))
-      await dropdown.findElement(By.xpath("//option[. = 'Yes']")).click()
+      await dropdown.findElement(By.css("*[value='Yes']")).click()
     }
     await driver.findElement(By.id("Clinical Software")).click()
     {
       const dropdown = await driver.findElement(By.id("Clinical Software"))
-      await dropdown.findElement(By.xpath("//option[. = 'Dental 4 Windows']")).click()
+      await dropdown.findElement(By.css("*[value='Dental 4 Windows']")).click()
     }
     await driver.findElement(By.id("PharmacySoftware*")).click()
     {
       const dropdown = await driver.findElement(By.id("Pharmacy Software"))
-      await dropdown.findElement(By.xpath("//option[. = 'MyPractice']")).click()
+      await dropdown.findElement(By.css("*[value='MyPractice']")).click()
     }
     await driver.findElement(By.id("Secure Messaging Solution")).click()
     {
       const dropdown = await driver.findElement(By.id("Secure Messaging Solution"))
-      await dropdown.findElement(By.xpath("//option[. = 'MD Exhange']")).click()
+      await dropdown.findElement(By.css("*[value='MD Exhange']")).click()
     }
     await driver.findElement(By.id("Electronic Prescription Software")).click()
     {
       const dropdown = await driver.findElement(By.id("Electronic Prescription Software"))
-      await dropdown.findElement(By.xpath("//option[. = 'eRX']")).click()
+      await dropdown.findElement(By.css("*[value='eRX']")).click()
     }
     await driver.findElement(By.id("Health Provider Identifier Organisation")).click()
     await driver.findElement(By.id("Health Provider Identifier Organisation")).sendKeys("selenium1")
@@ -65,79 +65,79 @@ describe('Census', function() {
     await driver.findElement(By.id("Corporate Practice")).click()
     {
       const dropdown = await driver.findElement(By.id("Corporate Practice"))
-      await dropdown.findElement(By.xpath("//option[. = 'Yes']")).click()
+      await dropdown.findElement(By.css("*[value='Yes']")).click()
     }
     await driver.findElement(By.id("PIPs Claimed")).click()
     {
       const dropdown = await driver.findElement(By.id("PIPs Claimed"))
-      await dropdown.findElement(By.xpath("//option[. = 'Aged Care Access Initiative']")).click()
+      await dropdown.findElement(By.css("*[value='Aged Care Access Initiative']")).click()
     }
     await driver.findElement(By.id("PIPs Incentive Level")).click()
     {
       const dropdown = await driver.findElement(By.id("PIPs Incentive Level"))
-      await dropdown.findElement(By.xpath("//option[. = 'Asthma']")).click()
+      await dropdown.findElement(By.css("*[value='Asthma']")).click()
     }
     await driver.findElement(By.id("SIPs Claimed")).click()
     {
       const dropdown = await driver.findElement(By.id("SIPs Claimed"))
-      await dropdown.findElement(By.xpath("//option[. = 'Cervical Screening']")).click()
+      await dropdown.findElement(By.css("*[value='Cervical Screening']")).click()
     }
     await driver.findElement(By.id("Recall Patients for Health Assessments")).click()
     {
       const dropdown = await driver.findElement(By.id("Recall Patients for Health Assessments"))
-      await dropdown.findElement(By.xpath("//option[. = 'No']")).click()
+      await dropdown.findElement(By.css("*[value='No']")).click()
     }
     await driver.findElement(By.id("Provide Training for Nursing Students")).click()
     {
       const dropdown = await driver.findElement(By.id("Provide Training for Nursing Students"))
-      await dropdown.findElement(By.xpath("//option[. = 'Yes']")).click()
+      await dropdown.findElement(By.css("*[value='Yes']")).click()
     }
     await driver.findElement(By.id("Provide Training for GP Registrars")).click()
     {
       const dropdown = await driver.findElement(By.id("Provide Training for GP Registrars"))
-      await dropdown.findElement(By.xpath("//option[. = 'Yes']")).click()
+      await dropdown.findElement(By.css("*[value='Yes']")).click()
     }
     await driver.findElement(By.id("Training for Medical Students")).click()
     {
       const dropdown = await driver.findElement(By.id("Training for Medical Students"))
-      await dropdown.findElement(By.xpath("//option[. = 'Yes']")).click()
+      await dropdown.findElement(By.css("*[value='Yes']")).click()
     }
     await driver.findElement(By.id("Recall or Reminder System")).click()
     {
       const dropdown = await driver.findElement(By.id("Recall or Reminder System"))
-      await dropdown.findElement(By.xpath("//option[. = 'Breast Cancer Screening']")).click()
+      await dropdown.findElement(By.css("*[value='Breast Cancer Screening']")).click()
     }
     await driver.findElement(By.id("Bulk Billing Options for Customers")).click()
     {
       const dropdown = await driver.findElement(By.id("Bulk Billing Options for Customers"))
-      await dropdown.findElement(By.xpath("//option[. = 'No']")).click()
+      await dropdown.findElement(By.css("*[value='No']")).click()
     }
     await driver.findElement(By.id("Preferred Pathology Provider")).click()
     {
       const dropdown = await driver.findElement(By.id("Preferred Pathology Provider"))
-      await dropdown.findElement(By.xpath("//option[. = 'Laverty']")).click()
+      await dropdown.findElement(By.css("*[value='Laverty']")).click()
     }
     await driver.findElement(By.id("Allied Health Services")).click()
     {
       const dropdown = await driver.findElement(By.id("Allied Health Services"))
-      await dropdown.findElement(By.xpath("//option[. = 'Acupuncture']")).click()
+      await dropdown.findElement(By.css("*[value='Acupuncture']")).click()
     }
     await driver.findElement(By.id("Mammogram Referrals")).click()
     {
       const dropdown = await driver.findElement(By.id("Mammogram Referrals"))
-      await dropdown.findElement(By.xpath("//option[. = 'Campsie Health Centre']")).click()
+      await dropdown.findElement(By.css("*[value='Campsie Health Centre']")).click()
     }
     await driver.findElement(By.id("Track ATSI Status")).click()
     {
       const dropdown = await driver.findElement(By.id("Track ATSI Status"))
-      await dropdown.findElement(By.xpath("//option[. = 'Yes']")).click()
+      await dropdown.findElement(By.css("*[value='Yes']")).click()
     }
     await driver.sleep(3000)
     await driver.findElement(By.css(".wizard-btn-next")).click()
     await driver.findElement(By.id("QIP Accreditation")).click()
     {
       const dropdown = await driver.findElement(By.id("QIP Accreditation"))
-      await dropdown.findElement(By.xpath("//option[. = 'No']")).click()
+      await dropdown.findElement(By.css("*[value='No']")).click()
     }
     await driver.findElement(By.css(".input-group > #QIPAccreditationExpiryDate")).click()
     await driver.findElement(By.css(".input-group > #QIPAccreditationExpiryDate")).sendKeys("25/11/2025")
