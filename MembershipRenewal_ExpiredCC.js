2fd43a66338892fd86d0840ed2c6cc8da96a3320
Diana2906 <55404991+Diana2906@users.noreply.github.com>
2019-09-23 11:16:31 +0300

    Update MembershipRenewal_ExpiredCC.js

--- MembershipRenewal_ExpiredCC.js
+++ MembershipRenewal_ExpiredCC.js
@@ -16,27 +16,21 @@ describe('Expired CC', function() {
   it('Expired CC', async function() {
     await driver.get("https://dev-cesphn.cs115.force.com/apex/membershiprenewal")
     await driver.sleep(4000)
-    await driver.findElement(By.id("memberNumber")).click()
     await driver.findElement(By.id("memberNumber")).sendKeys("201097")
-    await driver.findElement(By.id("memberEmail")).click()
     await driver.findElement(By.id("memberEmail")).sendKeys("developer+selenium@vertic.com.au")
     await driver.sleep(5000)
     await driver.findElement(By.id("cardNumber")).sendKeys("4444 3333 2222 1111")
-    await driver.findElement(By.id("customerName")).click()
     await driver.findElement(By.id("customerName")).sendKeys("selenium")
-    await driver.findElement(By.id("Expiry Month")).click()
     {
       const dropdown = await driver.findElement(By.id("Expiry Month"))
       await dropdown.findElement(By.css("*[value='01']")).click()
     }
-    await driver.findElement(By.id("Expiry Year")).click()
     {
       const dropdown = await driver.findElement(By.id("Expiry Year"))
       await dropdown.findElement(By.css("*[value='2019']")).click()
     }
-    await driver.findElement(By.id("CVN")).click()
     await driver.findElement(By.id("CVN")).sendKeys("123")
     await driver.findElement(By.css(".rsform-submit-button")).click()
-    await driver.wait(until.elementLocated(By.css(".toast-error")), 20000)
+    await driver.wait(until.elementLocated(By.css(".toast-error")), 35000)
   })
 })
