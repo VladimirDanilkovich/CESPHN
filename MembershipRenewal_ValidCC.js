e448ed2527f2a453d3571ab020bff3660d391511
Diana2906 <55404991+Diana2906@users.noreply.github.com>
2019-09-23 12:39:00 +0300

    Update MembershipRenewal_ValidCC.js

--- MembershipRenewal_ValidCC.js
+++ MembershipRenewal_ValidCC.js
@@ -3,7 +3,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
 describe('Valid CC', function() {
-  this.timeout(50000)
+  this.timeout(60000)
   let driver
   let vars
   beforeEach(async function() {
