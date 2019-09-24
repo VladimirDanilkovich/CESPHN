51bffb807cde6a40fec4240fbfa9346f0142b19d
Diana2906 <55404991+Diana2906@users.noreply.github.com>
2019-09-24 08:45:02 +0300

    Update MembershipRenewal_ExpiredCC.js

--- MembershipRenewal_ExpiredCC.js
+++ MembershipRenewal_ExpiredCC.js
@@ -3,7 +3,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
 describe('Expired CC', function() {
-  this.timeout(50000)
+  this.timeout(80000)
   let driver
   let vars
   beforeEach(async function() {
