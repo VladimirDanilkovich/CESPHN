e492fbf9559f14b934507f42042bbf37551340ff
Diana2906 <55404991+Diana2906@users.noreply.github.com>
2019-09-24 09:43:03 +0300

    Update MembershipSignUp_ExpiredCC_AlliedHealthProvider.js

--- MembershipSignUp_ExpiredCC_AlliedHealthProvider.js
+++ MembershipSignUp_ExpiredCC_AlliedHealthProvider.js
@@ -3,7 +3,7 @@ const { Builder, By, Key, until } = require('selenium-webdriver')
 const assert = require('assert')
 
 describe('Expired CC (Allied Health Provider)', function() {
-  //this.timeout(60000)
+  this.timeout(80000)
   let driver
   let vars
   beforeEach(async function() {
